c9d4ecdd94e4aabd1a47373424cba6e49e6b61e0
richard <richard@cardgate.com>
2019-10-23 14:45:46 +0200

    New payment method: OnlineÜberweisen.

--- app/code/local/Cardgate/Cgp/Model/Gateway/Onlineueberweisen.php
+++ app/code/local/Cardgate/Cgp/Model/Gateway/Onlineueberweisen.php
@@ -8,7 +8,7 @@
  */
 class Cardgate_Cgp_Model_Gateway_Onlineueberweisen extends Cardgate_Cgp_Model_Gateway_Abstract
 {
-
+ 
 	protected $_code = 'cgp_onlineueberweisen';
 
 	protected $_model = 'onlineueberweisen';
